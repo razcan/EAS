92042de3ea9cf27319a36cad823bf093a4b11ee5
Razvan Mustata <razvan.mustata@gmail.com>
2017-12-14 16:01:12 +0200

    setup primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,100 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core';   
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {CommonModule} from '@angular/common';
+import {MenuItem,MenuModule,ButtonModule,TabViewModule, InputTextModule, CheckboxModule, 
+  RadioButtonModule, SelectItem, TabMenuModule, DropdownModule, AccordionModule, PanelMenuModule} from 'primeng/primeng';
+import {NgModule} from '@angular/core';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+import {CodeHighlighterModule} from 'primeng/primeng';
 
+interface City {
+  name: string,
+  code: string
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+
+export class AppComponent implements OnInit {
   title = 'app';
+  items: MenuItem[];
+  
+      ngOnInit() {
+          this.items = [
+              {
+                  label: 'File',
+                  icon: 'fa-file-o',
+                  items: [{
+                          label: 'New', 
+                          icon: 'fa-plus',
+                          items: [
+                              {label: 'Project'},
+                              {label: 'Other'},
+                          ]
+                      },
+                      {label: 'Open'},
+                      {separator: true},
+                      {label: 'Quit'}
+                  ]
+              },
+              {
+                  label: 'Edit',
+                  icon: 'fa-edit',
+                  items: [
+                      {label: 'Undo', icon: 'fa-mail-forward'},
+                      {label: 'Redo', icon: 'fa-mail-reply'}
+                  ]
+              },
+              {
+                  label: 'Help',
+                  icon: 'fa-question',
+                  items: [
+                      {
+                          label: 'Contents'
+                      },
+                      {
+                          label: 'Search', 
+                          icon: 'fa-search', 
+                          items: [
+                              {
+                                  label: 'Text', 
+                                  items: [
+                                      {
+                                          label: 'Workspace'
+                                      }
+                                  ]
+                              },
+                              {
+                                  label: 'File'
+                              }
+                      ]}
+                  ]
+              },
+              {
+                  label: 'Actions',
+                  icon: 'fa-gear',
+                  items: [
+                      {
+                          label: 'Edit',
+                          icon: 'fa-refresh',
+                          items: [
+                              {label: 'Save', icon: 'fa-save'},
+                              {label: 'Update', icon: 'fa-save'},
+                          ]
+                      },
+                      {
+                          label: 'Other',
+                          icon: 'fa-phone',
+                          items: [
+                              {label: 'Delete', icon: 'fa-minus'}
+                          ]
+                      }
+                  ]
+              }
+          ];
+      }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
-
+import { AccordionModule,MenuItem,MenuModule,ButtonModule,TabViewModule, InputTextModule, CheckboxModule, 
+  RadioButtonModule,CodeHighlighterModule, SelectItem, TabMenuModule, DropdownModule, Button,PanelMenuModule
+ }  from 'primeng/primeng';     
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {CommonModule} from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {Routes } from "@angular/router";
+import { HttpModule, JsonpModule } from '@angular/http';
+import {CalendarModule} from 'primeng/primeng';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    CommonModule,
+    FormsModule,
+    DropdownModule,
+    TabViewModule,
+    MenuModule,
+    TabMenuModule,
+    HttpModule,
+    AccordionModule, InputTextModule, CheckboxModule, 
+    RadioButtonModule,CodeHighlighterModule, TabMenuModule,CalendarModule,PanelMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
